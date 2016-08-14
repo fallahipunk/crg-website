2fe156ffd7a225055f704d428349983c4001b5d2
Haitham Ennasr <haitham@llcl.lc>
2016-08-14 15:28:43 -0400

    replaced mockups with base Sage, added a bootstrap navbar and a base susbcription template

---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php // oroginally copied from page.php 
+	while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
+<?php get_template_part('templates/form-newsletter'); // copied the form example from the book, will change it later
+ ?>
