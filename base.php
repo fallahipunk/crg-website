282307778bd2686ad772434979e2f180c8383243
Haitham Ennasr <haitham@llcl.lc>
2016-08-15 15:28:13 -0400

    started working on homepage

--- base.php
+++ base.php
@@ -18,8 +18,14 @@ use Roots\Sage\Wrapper;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container" role="document">
+	
+	<?php if (is_front_page()) :  // adding if statment for front page layout ?>
+    <div class="wrap container front-page" role="document">
+			 <div class="content row front-page">
+		<?php else: ?>
+		<div class="wrap container" role="document">
       <div class="content row">
+		<?php endif; ?>
         <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,10 @@
+<?php if (is_front_page()) : ?>
+	
+	<div class= "front-page-container">
+	<?php the_content(); ?>
+	</div>
+
+<?php else: ?>
 <?php the_content(); ?>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<?php endif; ?>
