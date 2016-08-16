9841f2c7f5ad33b365adc48a77cb4e73c8624a12
magicdarvin <magicdarvin@gmail.com>
2016-08-17 01:35:00 +0300

    aspect ratio + logo
    proper aspect ratio for front page container image + logo to the left
    side

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<?php // oroginally copied from page.php 
+<?php // oroginally copied from page.php
 	while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
 ?>
 
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
-  <div class="container">
+  <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
@@ -14,12 +14,12 @@
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
-		  <?php //bloginfo('name'); // we need to change the following logo image when switching from mobile to desktop ?> 
+		  <?php //bloginfo('name'); // we need to change the following logo image when switching from mobile to desktop ?>
 		  <img src="<?= get_template_directory_uri() . '/dist/images/logo-sm.jpg'; ?>">
 	  </a>
     </div>
 
-    <nav class="collapse navbar-collapse" role="navigation">
+     <nav class="collapse" role="navigation">  <!-- removed class navbar-collapse so the logo white background won't stretch to 100% -->
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
