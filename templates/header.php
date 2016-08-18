c5aaf2a5b59fee60f312ad7a7dcb7d8668498a9c
magicdarvin <magicdarvin@gmail.com>
2016-08-18 15:50:41 +0300

    logo png now works
    logo png now works

--- templates/header.php
+++ templates/header.php
@@ -15,8 +15,8 @@
       </button>
       <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
 
-		  <?php //bloginfo('name'); // we need to change the following logo image when switching from mobile to desktop ?> 
-		  <img src="<?= get_template_directory_uri() . '/dist/images/logo-sm.png'; ?>">
+		  <?php //bloginfo('name'); // we need to change the following logo image when switching from mobile to desktop ?>
+		  <img src="<?= get_template_directory_uri() . '/assets/images/logo-sm.png'; ?>">
 
 	  </a>
     </div>
