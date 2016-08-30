e5cc86fff5eaa39d27e2513eb2de0e2369f6a8f0
Haitham Ennasr <haitham@llcl.lc>
2016-08-30 14:21:44 -0400

    address section of front page done

--- front-page.php
+++ front-page.php
@@ -22,28 +22,30 @@
 	<div class="row">  
 		<!--  address -->  <div   class="col-xs-12 col-sm-6 col-md-4"> 
 			 <div class="address"> 
-				 195 Chrystie Street, New York, NY 10002
+				 195 Chrystie Street. New York, NY. 10002<br>
+				 T - 212 229 2766<br>
+				 info@crggallery.com<br>
+				 <br>
 			 </div> 
 		 </div>
 
 		<!--  google map -->  <div   class="col-xs-12 col-sm-6 col-md-4"> 
 			 <div class="google-map"> 
-				 google map
+				 
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.813938211398!2d-73.99432128539928!3d40.72211227933079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259c7f0a4a6d1%3A0xeecfca023ef23f1b!2sCRG+Gallery!5e0!3m2!1sen!2sus!4v1472579612493" width=100% height=100% frameborder="0" style="border:0" allowfullscreen></iframe>
+				 
 			 </div> 
 		 </div>
   
 		<!--   hours-->  <div   class="col-xs-12 col-md-4"> 
 			 <div class="hours"> 
-				 T - 212 229 2766
-				 info@crggallery.com
-
-				 Summer Schedule
-				 June / July
-				 Tuesday - Friday
-				 11am - 6pm
-
-				 August
-				 By appointment
+				 Summer Schedule<br>
+				 June / July<br>
+				 Tuesday - Friday<br>
+				 11am - 6pm<br>
+				 <br>
+				 August<br>
+				 By appointment<br>
 			 </div> 
 		 </div>
 	</div>
