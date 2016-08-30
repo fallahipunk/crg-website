82f5e6f2a87abc819561d8680e1ea19f630004f1
Haitham Ennasr <haitham@llcl.lc>
2016-08-30 16:58:29 -0400

    updated mailing list for and added social media logos

--- templates/form-newsletter.php
+++ templates/form-newsletter.php
@@ -1,5 +1,5 @@
 Subscribe to our mailing list:
-<form action="https://example.com/subscribe" method="post">
+<form action="https://app.e2ma.net/app/view:Join/acctId:1353371/signupId:1364723" method="post">
   <input type="email" value="" name="email" placeholder="Your email">
   <button class="btn btn-link" type="submit"><span class="glyphicon glyphicon-ok-circle"></button>
 </form>
