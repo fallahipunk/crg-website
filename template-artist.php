acfd59c2a0df4faf28e4c30cc6243461f1e2576b
magicdarvin <magicdarvin@gmail.com>
2016-09-20 01:55:11 +0300

    arrows, expand-collapse, grid-adjustments
    what to do with press?

--- template-artist.php
+++ template-artist.php
@@ -4,8 +4,43 @@
  */
 $pid = $post->ID;
 $postname = $post->post_title;
-
 ?>
+<script>
+jQuery( document ).ready(function($) {
+  $('.more-biography').toggle(function() {
+      $('.artist-biography').addClass("expand");
+      $('.expand-biography').removeClass("glyphicon-menu-down").addClass("glyphicon-menu-up");
+      $('.cheat').removeClass("clearfix");
+
+    }, function () {
+  $(".artist-biography").removeClass("expand");
+  $('.expand-biography').removeClass("glyphicon-menu-up").addClass("glyphicon-menu-down");
+  $('.cheat').addClass("clearfix");
+  });
+
+//Press-expand
+  $('.more-press').toggle(function() {
+      $('.artist-press').addClass("expand");
+      $('.expand-press').removeClass("glyphicon-menu-down").addClass("glyphicon-menu-up");
+
+    }, function () {
+  $(".artist-press").removeClass("expand");
+  $('.expand-press').removeClass("glyphicon-menu-up").addClass("glyphicon-menu-down");
+  });
+
+  //Events-expand
+  $('.more-events').toggle(function() {
+      $('.artist-events').addClass("expand");
+      $('.expand-events').removeClass("glyphicon-menu-down").addClass("glyphicon-menu-up");
+
+    }, function () {
+  $(".artist-events").removeClass("expand");
+  $('.expand-events').removeClass("glyphicon-menu-up").addClass("glyphicon-menu-down");
+  });
+
+})
+</script>
+
 <?php while (have_posts()) : the_post(); ?>
   <!-- Image for Header -->
   <div class="artist-page-container">
@@ -16,62 +51,27 @@ $postname = $post->post_title;
   <div class="container">
   <!-- <?php get_template_part('templates/content', 'page'); ?> -->
     <div class="row">
-      <div class="col-xs-12 col-lg-8">
+
+      <!-- Selected work -->
+      <div class="col-lg-8">
         <h4>Selected Work</h4>
         <img id="artist-page-carousel-demo" src="<?php echo get('selected_image'); ?>">
   <!-- <?php echo get('selected_caption'); ?> -->
       </div>
-      <div class="col-xs-12 col-sm-6 col-md-8 col-lg-4" id="artist-biography">
+
+      <!-- Biography -->
+      <div class="col-xs-12 col-sm-10 col-md-6 col-lg-4 pull-right-lg">
         <h4>Biography</h4>
+        <div class="artist-biography">
         <?=get('bio');?>
-      </div>
-      
+        </div>
+        <button  type="button" class="btn btn-link more-biography pull-right" >
+          <span class="glyphicon glyphicon-menu-down expand-biography expand-arrow"></span>
+        </button>
+        </div>
+        <div class="cheat clearfix visible-lg-block"></div>
       <!-- Press -->
-      <div class="col-lg-4">
-        <h4>Press</h4>
-        <?
-
-        if ($pressID) :
-          query_posts('name='.$pressID.'&post_type=page');
-          the_post(); ?>
-
-          <table border="0" cellspacing="0" cellpadding="0" width="480">
-              <tr>
-                <td class="copy">
-                  <a class="print" href="#" onclick="window.print();"><img src="<?=FILES;?>/img/print.gif"></a>
-
-                  <br><span class="print">&nbsp;</span><br><span class="print">&nbsp;</span><br>
-
-                  <? if (get('url')) : ?>
-                  <a href="<?=get('url');?>" target="_blank"><? the_title();?></a>
-                  <? else : ?>
-                  <? the_title();?>
-                  <? endif;?>
-                  <br><? the_time('F d, Y');?>
-                  <?php edit_post_link("[edit]", "<br/>"); ?>
-                  </td>
-              </tr>
-
-              <tr>
-                <td><img src="<?=FILES;?>/img/sp.gif" width="1" height="30" alt=""></td>
-              </tr>
-              <tr>
-                <td class="divider"><img src="<?=FILES;?>/img/sp.gif" width="1" height="6" alt=""></td>
-              </tr>
-              <tr>
-                <td><img src="<?=FILES;?>/img/sp.gif" width="1" height="30" alt=""></td>
-              </tr>
-
-              <tr>
-                <td class="copy">
-                <div class="content"><?=the_content();?></div>
-                </td>
-
-              </tr>
-            </table>
-
-        <? else :
-
+      <?
           query_posts('post_parent=61&meta_key=artist&meta_value='.$pid.'&orderby=date&order=DESC&showposts=-1&post_type=page');
 
           if (have_posts()) : while (have_posts()) : the_post();
@@ -86,9 +86,11 @@ $postname = $post->post_title;
 
           );  endwhile; endif; wp_reset_query();?>
 
-
-
-          			<? if (is_array($press)) :
+          			<? if (is_array($press)) : ?>
+                <div class="col-md-6 col-lg-4">
+                  <h4>Press</h4>
+                  <div class="artist-press">
+                  <?
           						foreach ($press as $year => $pressArray) : //print_r($exhibition);
 
           						$count = 1;
@@ -99,92 +101,73 @@ $postname = $post->post_title;
           					?>
 
           		<div style="margin-bottom:10px;"><b><?=$yearGroup;?></b></div>
-          <div style="margin-bottom:10px;"><b><a href="?press=<?=$press['id'];?>" class="mmactive"><?=$press['title'];?></a></b> <?php edit_post_link("[edit]", "<br/>"); ?><span class="exdate"><i><?=$press['subhead'];?></i> <?=$press['date'];?></span></div>
+              <div style="margin-bottom:10px;"><b><a href="<?=$press['id'];?>"><?=$press['title'];?></a></b> <?php edit_post_link("[edit]", "<br/>"); ?><span class="exdate"><i><?=$press['subhead'];?></i> <?=$press['date'];?></span></div>
 
-          					<? endforeach; endforeach; endif; ?>
+            <? endforeach; endforeach; ?>
+          </div>
+            <button  type="button" class="btn btn-link more-press pull-right" >
+              <span class="glyphicon glyphicon-menu-down expand-arrow expand-press"></span>
+            </button>
+          </div>
 
-      <? endif ?>
+          <? endif; ?>
+
+      <!-- Publications -->
+      <?
+      query_posts('post_parent=35&meta_key=related_artist&meta_value='.$pid.'&orderby=date&order=DESC&showposts=-1&post_type=page');
+
+      if (have_posts()) : while (have_posts()) : the_post();
+
+      $args = array("h" => 150, "w" => 130);
+      $itemsArray[] = array(
+        'title'=>get_the_title(),
+        'artists'=>get_field_duplicate('related_artist'),
+        'permalink'=>get_permalink(),
+        'thumb'=>get_image('image',1,1,1,NULL,$args),
+        'year'=>get('year')
+      );
+
+      endwhile; endif; wp_reset_query();
+
+      if(count($itemsArray)) :
+        $itemsChunk = array_chunk($itemsArray,3,true);
+        foreach ($itemsChunk as $items) :
+      ?>
+
+        <div class="col-md-6 col-lg-4">
+          <h4>Publications</h4>
+            <div class="row">
+              <? foreach ($items as $item) : //print_r($item);
+                $artists = "";
+                foreach ($item['artists'] as $artist) :
+                  $artists .= get_the_title($artist)."<br/>";
+                endforeach; ?>
+                <div class="col-lg-6">
+                  <a href="<?=$item['permalink'];?>"><?=$item['thumb'];?></a><br>
+                  <span><a href="<?=$item['permalink'];?>" class="mmactive"><?=$item['title'];?></a><span class="exdate"><br><?=$artists;?><?=$item['year'];?></span></span></div>
+                <? endforeach; ?>
+              </div>
+            <? endforeach; ?>
+          </div>
+        <? else: endif;?>
 
 
+      <!-- Events -->
+      <? query_posts('cat=1&meta_key=related_artist&meta_value='.$pid.'&orderby=date&order=DESC&showposts=-1');
+      if (have_posts()) : ?>
+      <div class="col-md-6 col-lg-4">
+        <h4>Events</h4>
+        <div class="artist-events">
+        <? while (have_posts()) : the_post(); ?>
+          <a href="<? the_permalink();?>"><? the_title(); ?></a><br/><br/>
+          <? edit_post_link("[edit]", "<br/>"); ?>
+        <? endwhile;?>
       </div>
-
-<!-- Publications -->
-<div class="col-lg-4">
-  <h4>Publications</h4>
-
-
-<?
-query_posts('post_parent=35&meta_key=related_artist&meta_value='.$pid.'&orderby=date&order=DESC&showposts=-1&post_type=page');
-
-if (have_posts()) : while (have_posts()) : the_post();
-
-$args = array("h" => 150, "w" => 130);
-$itemsArray[] = array(
-  'title'=>get_the_title(),
-  'artists'=>get_field_duplicate('related_artist'),
-  'permalink'=>get_permalink(),
-  'thumb'=>get_image('image',1,1,1,NULL,$args),
-  'year'=>get('year')
-  );
-
-endwhile; endif; wp_reset_query();
-
-
-if(count($itemsArray)) :
-
-$itemsChunk = array_chunk($itemsArray,3,true);
-
-
-foreach ($itemsChunk as $items) :
-?>
-<div class="row">
-<?
-
-
-foreach ($items as $item) :
-//print_r($item);
-$artists = "";
-foreach ($item['artists'] as $artist) :
-$artists .= get_the_title($artist)."<br/>";
-endforeach;
-
-?>
-  <div class="col-lg-6">
-  <a href="<?=$item['permalink'];?>"><?=$item['thumb'];?></a><br>
-  <span><a href="<?=$item['permalink'];?>" class="mmactive"><?=$item['title'];?></a><span class="exdate"><br><?=$artists;?><?=$item['year'];?></span></span></div>
-<?
-
-endforeach;
-
-
-?>
-</div>
-<?
-endforeach;
-
-else :
-
-echo "There are no Publications available at this time.";
-
-endif;
-?>
+      <button  type="button" class="btn btn-link more-events pull-right" >
+        <span class="glyphicon glyphicon-menu-down expand-arrow expand-events"></span>
+      </button>
+      </div>
+      <? endif; wp_reset_query();?>
 
 </div>
-
-<!-- Events -->
-<div class="col-lg-4">
-  <h4>Events</h4>
-  <?
-  query_posts('cat=1&meta_key=related_artist&meta_value='.$pid.'&orderby=date&order=DESC&showposts=-1');
-  if (have_posts()) : while (have_posts()) : the_post();
-  ?>
-
-    <a href="<? the_permalink();?>"><? the_title(); ?></a><br/>
-    <? the_time('m d, Y');?>
-    <? edit_post_link("[edit]", "<br/>"); ?>
-    <? endwhile; endif; wp_reset_query();?>
-  </div>
-
-  </div>
-<div>
 <?php endwhile; ?>
