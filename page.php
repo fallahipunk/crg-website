13aae91198ebfae11f2894611721fe16464c5ecf
magicdarvin <magicdarvin@gmail.com>
2016-10-06 03:33:08 +0300

    fixed print issue on press page + some improvements in general print template + added the print to press release section on exhibition page
    now print button is available for press + artist-bio + exhibition-press
    release

--- page.php
+++ page.php
@@ -1,14 +1,9 @@
 <?php
 if (61 == $post->post_parent) {
     include (TEMPLATEPATH . '/template-press.php'); // Name this for your child page template name
-    exit(); // - breaks scripts on the page
 } else {
-    // Do something else
-    // You might want to stick your regular page.php code in here, or alternatively, you could call another template
+while (have_posts()) : the_post(); ?>
+    <?php get_template_part('templates/page', 'header'); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+  <?php endwhile;
 }; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
--- template-exhibition.php
+++ template-exhibition.php
@@ -96,6 +96,7 @@ jQuery(document).ready(function($)
       <!-- Press Release -->
       <div class="col-xs-12 col-lg-4 col-lg-push-8">
         <h4>Press Release</h4>
+        <a class="print small" href="#" onclick='jQuery("#press-release").print({stylesheet:"/wp-content/themes/crg-website/dist/styles/print.css",prepend:"<div><h1><b>CRG Gallery</h1></b><small>195 Chrystie Street, New York, NY 10002</small>  |  <small>T – 212 229 2766, F – 212 229 2788</small>  |  <small>info@crggallery.com</small><div><br><h2><? the_title(); ?></h2><br><b>Press Release<b>"});'><span class="glyphicon glyphicon-print"></span> print</a>
         <div id="press-release">
         <? the_content();?>
         </div>
