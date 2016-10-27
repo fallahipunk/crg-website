2fb51913d39ba411b6dae711c8f39532eda7fb9f
Haitham Ennasr <haitham@llcl.lc>
2016-10-27 07:22:54 -0400

    changed the mailing list subscription form once again

--- templates/form-newsletter.php
+++ templates/form-newsletter.php
@@ -2,23 +2,59 @@ Subscribe to our mailing list:
 <form action="https://crggallery.us14.list-manage.com/subscribe/post" method="POST">
     <input type="hidden" name="u" value="b68a1278d1243c4b5b718a270">
     <input type="hidden" name="id" value="515c57370c">
-	
- 
-            <input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="" placeholder="Your Email*">
-            
+    
+
+    <!-- people should not fill these in and expect good things -->
+
+
+    <div id="mergeTable" class="mergeTable">
         
-	                <input type="text" name="MERGE1" id="MERGE1" size="25" value="" placeholder="First Name">
-
-
-	                <input type="text" name="MERGE2" id="MERGE2" size="25" value="" placeholder="Last Name">
+        
+        <div class="mergeRow dojoDndItem mergeRow-email" id="mergeRow-0">
+          
+            <div class="field-group">
+                <input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="" placeholder="Your Email*">
                 
-
+                
+            </div>
             
+        </div>
         
         
-  <button class="btn btn-link" type="submit"><span class="glyphicon glyphicon-ok-circle"></button>
-	   
+        
+        <div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-1">
+      
+            <div class="field-group">
+                <input type="text" name="MERGE1" id="MERGE1" size="25" value="" placeholder="First Name">
+                
+                
+            </div>
+            
+        </div>
+        
+        
+        
+        <div class="mergeRow dojoDndItem mergeRow-text" id="mergeRow-2">
+         
+            <div class="field-group">
+                <input type="text" name="MERGE2" id="MERGE2" size="25" value="" placeholder="Last Name">
+                
+                
+            </div>
+            
+        </div>
+        
+        
 
-	    <input type="hidden" name="ht" value="e9d15c24d31d79f8d5f0a5a8a4165d5bf1ebaa9e:MTQ3NzU2MDgzNC45MjQ=">
-	    <input type="hidden" name="mc_signupsource" value="hosted">
-	</form>
+
+        
+    </div>
+
+    
+
+    <div class="submit_container clear">
+        <button class="btn btn-link" type="submit"><span class="glyphicon glyphicon-ok-circle"></button>
+    </div>
+    <input type="hidden" name="ht" value="dfeb5c5e0d33b0d6b961243cea555487033ccebd:MTQ3NzU2NjE5My45ODAy">
+    <input type="hidden" name="mc_signupsource" value="hosted">
+</form>
